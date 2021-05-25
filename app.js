a62dcdcc52bcb894b4c754dd52754e55acb0d15e
smdsaran <saranthuppaki056@gmail.com>
2021-05-25 18:26:55 +0530

    Added Procfile , gitignore and port

--- app.js
+++ app.js
@@ -165,9 +165,12 @@ app.post("/work" , function(req , res) {
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-
-app.listen(3000 , function() {
-    console.log("Server is running on port 3000");
+app.listen(port , function() {
+    console.log("Server is running .....");
 });
